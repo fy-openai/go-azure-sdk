73c62236b8f7b6bd76854e0cb0eec9a879d7af98
Fangyuan Li <fy@openai.com>
2024-09-09 15:30:17 -0400

    use the patch

--- resource-manager/keyvault/2023-07-01/vaults/method_listbysubscription.go
+++ resource-manager/keyvault/2023-07-01/vaults/method_listbysubscription.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
 	"github.com/hashicorp/go-azure-sdk/sdk/client"
-	"github.com/hashicorp/go-azure-sdk/sdk/odata"
+	"github.com/fy-openai/go-azure-sdk/sdk/odata"
 )
 
 // Copyright (c) Microsoft Corporation. All rights reserved.
